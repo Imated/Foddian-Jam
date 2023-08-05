fb1b55aacf8845e18b3204db1570ab7e37a62d8f
RobotOverlord550 <erniewhitmarsh3@gmail.com>
2023-08-05 18:56:26 -0400

    Added pause menu

--- Foddian Jam/Assets/Scripts/PauseMenu.cs
+++ Foddian Jam/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject pauseMenu;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void resumeGame()
     {
-        
+        pauseMenu.SetActive(false);
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
