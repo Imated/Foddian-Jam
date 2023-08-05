b1ee5836054c82582b44b33f65877a1cc65816b2
Imated <lineborder122@gmail.com>
2023-08-05 15:31:03 -0400

    Speed Fragers

--- Foddian Jam/Assets/Scripts/BreakCheck.cs
+++ Foddian Jam/Assets/Scripts/BreakCheck.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BreakCheck : MonoBehaviour
 {
@@ -8,18 +10,24 @@ public class BreakCheck : MonoBehaviour
     [SerializeField] private float angleThreshold;
     [SerializeField] private float speedThreshold;
     [SerializeField] private AnimationCurve speedGainCurve;
+    [SerializeField] private GameObject speedFragmentPerfab;
+    [SerializeField] private Vector2 speedFragmentsRange;
+    [SerializeField] private float spedFragmentsRandomOffset = 2f;
     public float deflectionMod;
 
     [SerializeField] bool curBreak;
     [SerializeField] float _timer;
 
+    private Vector2 _direction;
+    private Vector2 _position;
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         ballCollider = GetComponent<Collider2D>();
-        // Gets velocity magnitude of exactly 4
-        //Vector2 force = Vector2.up * 200;
-        //rb.AddForce(force);
+
+        curBreak = false;
+        rb.velocity = new Vector2(0, 10);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -34,6 +42,7 @@ public class BreakCheck : MonoBehaviour
                 averageNormal += contact.normal;
             }
             averageNormal = averageNormal.normalized;
+            _direction = -averageNormal;
 
             // Angles closer to 0 are sharp collisions, 90 is parallel to surface
             float impactAngle = Vector2.Angle(relVelocity, averageNormal);
@@ -90,8 +99,25 @@ public class BreakCheck : MonoBehaviour
         if (collider.gameObject.CompareTag("Obstacle") && curBreak)
         {
             ballCollider.isTrigger = false;
+            _position = transform.position;
+            SpawnSpeedFragments();
             curBreak = false;
         }
         _timer = 0f;
+    }
+
+    private void SpawnSpeedFragments()
+    {
+        var amount = Random.Range(speedFragmentsRange.x, speedFragmentsRange.y);
+        for (int i = 0; i < amount; i++)
+        {
+            var dir = _direction;
+            dir.y = 1f;
+            dir.x += Random.Range(-0.25f, 0.25f);
+            var pos = _position;
+            pos.x += Random.Range(-spedFragmentsRandomOffset, spedFragmentsRandomOffset);
+            pos.y += Random.Range(0f, spedFragmentsRandomOffset);
+            Instantiate(speedFragmentPerfab, pos, Quaternion.identity).GetComponent<SpedFragment>().Go(dir, rb.velocity.magnitude);
+        }
     }
 }
