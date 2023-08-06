39d68bca060c036aae8a7ce93c81c447c112b679
Imated <lineborder122@gmail.com>
2023-08-06 18:02:11 -0400

    Game Over Stuff

--- Foddian Jam/Assets/Scripts/BoundaryManager.cs
+++ Foddian Jam/Assets/Scripts/BoundaryManager.cs
@@ -1,16 +1,20 @@
 using System;
+using Cinemachine;
+using DG.Tweening;
 using UnityEngine;
 using TMPro;
 
 public class BoundaryManager : MonoBehaviour
 {
-    [SerializeField] float boundaryBreakSpeed = 0;
-    [SerializeField] GameObject boundaries;
-    [SerializeField] GameObject timer;
-    [SerializeField] GameObject endGameUI;
-    [SerializeField] Animation endGameAnim;
-    [SerializeField] TMP_Text endGameText;
-    [SerializeField] LeaderboardManager3 leaderboard;
+    public static bool HasGameEnded;
+    
+    [SerializeField] private float boundaryBreakSpeed = 0;
+    [SerializeField] private GameObject boundaries;
+    [SerializeField] private GameObject timer;
+    [SerializeField] private GameObject endGameUI;
+    [SerializeField] private Animator endGameUIAnimator;
+    [SerializeField] private TMP_Text endGameText;
+    [SerializeField] private LeaderboardManager3 leaderboard;
 
     private void OnCollisionEnter2D(Collision2D col)
     {
@@ -20,14 +24,16 @@ public class BoundaryManager : MonoBehaviour
 
         if (rigidBody.velocity.magnitude >= boundaryBreakSpeed)
         {
+            timer.SetActive(false);
             endGameUI.SetActive(true);
-            endGameAnim.Play();
+            endGameUIAnimator.Play("End Game UI");
             endGameText.text = $"CONGRATULATIONS! \n" +
                                $"You have beaten the impossible in {(timer.GetComponent<Timer>().timer):F2} seconds.";
-            timer.GetComponent<Timer>().timerActive = false;
-            timer.SetActive(false);
+            print("a");
             leaderboard.StartSubmitScoreCoroutine((int) Math.Floor(timer.GetComponent<Timer>().timer)); // need this because this gameobject becomes inactive and will not run a coroutine
             boundaries.SetActive(false);
+            print("b");
+            HasGameEnded = true;
         }
     }
 }
