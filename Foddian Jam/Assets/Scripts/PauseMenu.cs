e23cc9139798a7ef860b037aa8e2d0fb211b0195
Imated <lineborder122@gmail.com>
2023-08-05 23:52:54 -0400

    Pause Menu Stuff

--- Foddian Jam/Assets/Scripts/PauseMenu.cs
+++ Foddian Jam/Assets/Scripts/PauseMenu.cs
@@ -6,20 +6,21 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] UIMoveY uiMover;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0;
+            uiMover.Tween();
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
         }
     }
 
     public void resumeGame()
     {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1;
+        uiMover.Tween();
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
     }
 
     public void mainMenu()
