b9863b4e66430c51cfc9e2c240ef60fb08106512
Imated <lineborder122@gmail.com>
2023-08-06 13:48:25 -0400

    Fixed Bugs

--- Foddian Jam/Assets/Scripts/MainMenu.cs
+++ Foddian Jam/Assets/Scripts/MainMenu.cs
@@ -2,29 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LootLocker.Requests;
+using UnityEngine.SceneManagement;
 
-public class LeaderboardManager2 : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    int leaderboardID = 16665;
-
-    public IEnumerator SubmitScoreRoutine(int scoreToUpload)
+    public void PlayGame()
     {
-        bool done = false;
-        string playerID = PlayerPrefs.GetString("PlaeyerID");
+        SceneManager.LoadScene("Level Scene");
+    }
 
-        LootLockerSDKManager.SubmitScore(playerID, scoreToUpload, leaderboardID.ToString(), (response) =>
-        {
-            if (response.success)
-            {
-                Debug.Log("Successfully uploaded score");
-                done = true;
-            }
-            else
-            {
-                Debug.Log("Failed" + response.Error);
-                done = true;
-            }
-        });
-        yield return new WaitWhile(() => done = false);
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
