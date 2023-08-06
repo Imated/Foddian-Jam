27cfdc2ebc113130e77ee07b266c08075ea4af8e
RobotOverlord550 <erniewhitmarsh3@gmail.com>
2023-08-05 23:57:11 -0400

    added leaderboard

--- Foddian Jam/Assets/Scripts/BoundaryManager.cs
+++ Foddian Jam/Assets/Scripts/BoundaryManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LootLocker.Requests;
 
 public class BoundaryManager : MonoBehaviour
 {
@@ -8,16 +10,18 @@ public class BoundaryManager : MonoBehaviour
     [SerializeField] GameObject boundaries;
     [SerializeField] GameObject timer;
     [SerializeField] GameObject victoryText;
+    [SerializeField] Leaderboard leaderboard;
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         Rigidbody2D rigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
 
-        if(rigidBody.velocity.magnitude >= boundaryBreakSpeed)
+        if (rigidBody.velocity.magnitude >= boundaryBreakSpeed)
         {
             boundaries.SetActive(false);
             victoryText.SetActive(true);
-            timer.GetComponent<Timer>().timerActive = false;   
+            timer.GetComponent<Timer>().timerActive = false;
+            StartCoroutine(leaderboard.SubmitScoreRoutine((int)Math.Floor(timer.GetComponent<Timer>().timer)));
         }
     }
 }
