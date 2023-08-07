432aa44d1f80bdd423076db606d582bf0dd3d92e
Imated <lineborder122@gmail.com>
2023-08-07 04:17:34 -0400

    Changes

--- Foddian Jam/Assets/Scripts/Spedometer.cs
+++ Foddian Jam/Assets/Scripts/Spedometer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Spedometer : MonoBehaviour
 {
+    [SerializeField] private Color completeColor = Color.green;
     [SerializeField] private Image fillImage;
     [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private float lerpSpeed = 1f;
@@ -25,6 +26,10 @@ public class Spedometer : MonoBehaviour
         if(!overwriteSpeed)
             value = playerRb.velocity.magnitude;
         var normalizedValue = value / max;
+        if (Math.Abs(normalizedValue - 1f) < 0.01f)
+            fillImage.color = completeColor;
+        else
+            fillImage.color = Color.white;
         var topValue = Mathf.Lerp(460, 30, normalizedValue);
         _currentValue = Mathf.Lerp(_currentValue, topValue, Time.deltaTime * lerpSpeed);
         _fillImageRect.offsetMax = new Vector2(_fillImageRect.offsetMax.x, -_currentValue);
