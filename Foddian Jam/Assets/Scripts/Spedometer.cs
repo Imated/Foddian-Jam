8511afe3f454112f2998f39668987ecdde94edc7
Imated <lineborder122@gmail.com>
2023-08-07 03:08:46 -0400

    Spedometer

---
+++ Foddian Jam/Assets/Scripts/Spedometer.cs
@@ -0,0 +1,32 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spedometer : MonoBehaviour
+{
+    [SerializeField] private Image fillImage;
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private float lerpSpeed = 1f;
+    [SerializeField] private float max = 100f;
+    [SerializeField] private bool overwriteSpeed = true;
+    [SerializeField] private float value;
+    
+    private RectTransform _fillImageRect;
+    private float _currentValue;
+
+    private void Awake()
+    {
+        _fillImageRect = fillImage.GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if(!overwriteSpeed)
+            value = playerRb.velocity.magnitude;
+        var normalizedValue = value / max;
+        var topValue = Mathf.Lerp(460, 30, normalizedValue);
+        _currentValue = Mathf.Lerp(_currentValue, topValue, Time.deltaTime * lerpSpeed);
+        _fillImageRect.offsetMax = new Vector2(_fillImageRect.offsetMax.x, -_currentValue);
+    }
+}
