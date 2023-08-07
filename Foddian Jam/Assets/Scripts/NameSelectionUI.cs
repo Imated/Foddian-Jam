17a70bfb9c8a4d32af7c56991e8fd5b0fa93e4e6
Imated <lineborder122@gmail.com>
2023-08-06 21:00:17 -0400

    Name Selection UI

---
+++ Foddian Jam/Assets/Scripts/NameSelectionUI.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class NameSelectionUI : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    
+    public void SetName()
+    {
+        
+    }
+}
